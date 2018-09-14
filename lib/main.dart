486e9f201b1745d558d35de84b42d53b19473d8c
ThaDocIzN <rodan72@msn.com>
2018-09-14 01:26:25 -0500

    Added a ChatScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new MaterialApp(
-      title: 'Friendlychat',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Friendlychat'),
-        ),
-      ),
-    ),
-  );
+  runApp(new FriendlyChatApp());
+}
+class FriendlyChatApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Friendly Chat',
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Friendly Chat')),
+    );
+  }
 }
 
